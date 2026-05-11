b36cab2f053782e5eca219a58d00af8749611b4d
Musiienko <mysiienko.vlad14062007@gmail.com>
2026-05-11 16:48:25 +0300

    interface

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,5 +1,13 @@
 import axios from "axios"
-import type { FetchMoviesResponse } from "../types/movie";
+import type { Movie } from "../types/movie";
+interface FetchMoviesResponse {
+  page: number;
+  results: Movie[];
+  total_pages: number;
+  total_results: number;
+}
+
+
 const api = axios.create({
   baseURL: 'https://api.themoviedb.org/3',
   headers: {
